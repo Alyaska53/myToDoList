ea8f445db9df77629390f409ac7a213505175db3
Dmitry Buravkin <dmitry.buravkin@gmail.com>
2022-05-20 17:53:30 +0300

    feat: add descending sort service

---
+++ src/services/sortDescending.js
@@ -0,0 +1,19 @@
+export function sortDescending(list) {
+  if (list.length <= 1) {
+    return list;
+  }
+
+  const base = list[list.length - 1];
+  const leftList = [];
+  const rightList = [];
+
+  for (let i = 0; i < list.length - 1; i++) {
+    if (list[i].id < base.id) {
+      leftList.push(list[i]);
+    } else {
+      rightList.push(list[i]);
+    }
+  }
+
+  return [...sortDescending(leftList), base, ...sortDescending(rightList)];
+}
